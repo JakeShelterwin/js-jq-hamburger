b41e3e22d32afd1acb820433e4a9ada6fd0831dd
Giacomo <kirtash.k@live.it>
2020-03-31 15:07:09 +0200

    Esercizio Funzionante

---
+++ main.js
@@ -0,0 +1,17 @@
+// DESCRIZIONE:
+// Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript/jQuery
+// prendo lo zip che dopo vi allego e senza toccare l’HTML faccio funzionare l’hamburger menù;
+// primo click, appare il menù;
+// se cliccko che è già visibile, invece scompare.
+
+$('.fa-bars').click(
+  function(){
+    $('.hamburger-menu').show();
+  }
+);
+
+$('.fa-times').click(
+  function(){
+    $('.hamburger-menu').hide();
+  }
+);
